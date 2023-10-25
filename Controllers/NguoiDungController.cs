2acf126f4cbc749a65e9fcdc5306a976ee0d4500
Java436 <141116500+Java436@users.noreply.github.com>
2023-10-25 12:09:56 +0700

    Add files via upload

---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebFnB.Models;
+
+namespace WebFnB.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        QLBANHANGEntities database = new QLBANHANGEntities();
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        // GET: NguoiDung
+        [HttpPost]
+        public ActionResult DangKy(KH kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.TenKH))
+                    ModelState.AddModelError(string.Empty, "Họ tên không được để trống");
+                if (string.IsNullOrEmpty(kh.TaiKhoan))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.MatKhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(kh.Email))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(kh.SDT))
+                    ModelState.AddModelError(string.Empty, "Điện thoại không được để trống");
+                //Kiểm tra xem có người nào đã đăng kí với tên đăng nhập này hay chưa
+                var khachhang = database.KHs.FirstOrDefault(k => k.TaiKhoan ==
+                kh.TaiKhoan);
+                if (khachhang != null)
+                    ModelState.AddModelError(string.Empty, "Đã có người đăng kí tên này");
+                if (ModelState.IsValid)
+                {
+                    database.KHs.Add(kh);
+                    database.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("DangNhap");
+        }
+        //Phần đăng nhập//
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        //Phần đăng nhập//
+        [HttpPost]
+        public ActionResult DangNhap(KH kh)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(kh.TaiKhoan))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(kh.MatKhau))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (ModelState.IsValid)
+                {
+                    //Tìm khách hàng có tên đăng nhập và password hợp lệ trong CSDL
+                    var khach = database.KHs.FirstOrDefault(k => k.TaiKhoan ==
+                    kh.TaiKhoan && k.MatKhau == kh.MatKhau);
+                    if (khach != null)
+                    {
+                        ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                        //Lưu vào session
+                        Session["TaiKhoan"] = khach;
+                        return RedirectToAction("Index","Home");
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
