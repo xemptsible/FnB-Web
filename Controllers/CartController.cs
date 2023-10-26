80af30afe544d1c795e32affe22cccd280eff747
Nguyễn Khánh <khanhpoke223@gmail.com>
2023-10-26 11:51:34 +0700

    hmmm

---
+++ Controllers/CartController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebFnB.Models;
+
+namespace WebFnB.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public List<MatHangMua> LayGioHang()
+        {
+            List<MatHangMua> gioHang = Session["GioHang"] as List<MatHangMua>;
+            if (gioHang == null)
+            {
+                gioHang = new List<MatHangMua>();
+                Session["GioHang"] = gioHang;
+            }
+            return gioHang;
+        }
+        public ActionResult ThemSanPhamVaoGio(int MaSP)
+        {
+            List<MatHangMua> gioHang = LayGioHang();
+            MatHangMua sanPham = gioHang.FirstOrDefault(s => s.MaSP == MaSP);
+            if (sanPham == null)
+            {
+                sanPham = new MatHangMua(MaSP);
+                gioHang.Add(sanPham);
+            }
+            else
+            {
+                sanPham.SoLuong++;
+            }
+            return RedirectToAction("Details", "Product", new { id = MaSP });
+        }
+        private int TinhTongSL()
+        {
+            int tongSL = 0;
+            List<MatHangMua> gioHang = LayGioHang();
+            if (gioHang != null)
+                tongSL = gioHang.Sum(sp => sp.SoLuong);
+            return tongSL;
+        }
+        private decimal TinhTongTien()
+        {
+            decimal tongTien = 0;
+            List<MatHangMua> gioHang = LayGioHang();
+            if (gioHang != null)
+                tongTien = gioHang.Sum(sp => sp.ThanhTien());
+            return tongTien;
+        }
+        public ActionResult HienThiGioHang()
+        {
+            List<MatHangMua> gioHang = LayGioHang();
+            if (gioHang == null || gioHang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+            ViewBag.TongSL = TinhTongSL();
+            ViewBag.TongTien = TinhTongTien();
+            return View(gioHang);
+        }
+        public ActionResult GioHangPartial()
+        {
+            ViewBag.TongSL = TinhTongSL();
+            ViewBag.TongTien = TinhTongTien();
+            return PartialView();
+        }
+        public ActionResult DatHang()
+        {
+            if (Session["TaiKhoan"] == null)
+                return RedirectToAction("DangNhap", "NguoiDung");
+            List<MatHangMua> gioHang = LayGioHang();
+            if (gioHang == null || gioHang.Count == 0)
+                return RedirectToAction("Index", "BookStore");
+
+            ViewBag.TongSL = TinhTongSL();
+            ViewBag.TongTien = TinhTongTien();
+            return View(gioHang);
+        }
+    }
+}
