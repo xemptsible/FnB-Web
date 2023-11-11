050073d52ef5c9d05b050300eeae86f3cdb78fe6
Hongnha <hongnha1710@gmail.com>
2023-11-11 20:39:01 +0700

    chinhsua_layout

---
+++ Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFnB.Models
+{
+    public partial class Comment
+    {
+        public int ID { get; set; }
+        public int Rating { get; set; }
+        public string MoTa { get; set; }
+        public Nullable<System.DateTime> NgayDG { get; set; }
+        public int MaKH { get; set; }
+        public int MaSP { get; set; }
+    }
+}
